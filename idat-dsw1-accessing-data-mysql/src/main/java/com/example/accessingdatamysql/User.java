31f7c12fe9343bad109327e6164f256a7ce35653
DavidCaldas934 <davidcaldas934@gmail.com>
2023-06-12 15:09:07 -0500

    cambios

---
+++ idat-dsw1-accessing-data-mysql/src/main/java/com/example/accessingdatamysql/User.java
@@ -0,0 +1,52 @@
+package com.example.accessingdatamysql;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+//@Table( = "user")
+public class User {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+
+  private String pet;
+
+  private String raza;
+
+  private String duenio;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getPet() {
+    return pet;
+  }
+
+  public void setPet(String pet) {
+    this.pet = pet;
+  }
+
+  public String getRaza() {
+    return raza;
+  }
+
+  public void setRaza(String raza) {
+    this.raza = raza;
+  }
+
+  public String getDuenio() {
+    return duenio;
+  }
+
+  public void setDuenio(String duenio) {
+    this.duenio = duenio;
+  }
+}
